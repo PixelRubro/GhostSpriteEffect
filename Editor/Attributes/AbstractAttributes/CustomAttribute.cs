f61cddbb7227d2f86e13efeae16e2e89ef0a5b5a
msavioti <matheussavioti@gmail.com>
2023-09-19 01:48:42 -0300

    CHORE: resetting repo

--- Editor/Attributes/AbstractAttributes/CustomAttribute.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace VermillionVanguard.GhostSprite.InspectorAttributes
-{
-    [System.AttributeUsage(System.AttributeTargets.All, Inherited = false, AllowMultiple = true)]
-    public abstract class CustomAttribute : PropertyAttribute
-    {
-    }
-}
